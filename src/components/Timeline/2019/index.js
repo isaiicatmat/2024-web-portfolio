67f6a08ecd72ffc0ecf3d9841c578d6568b0ebe3
isaiicatmat <isaiicatmat@gmail.com>
2024-07-09 20:47:03 -0600

    fixed: typo on Seoul

--- src/components/Timeline/2019/index.js
+++ src/components/Timeline/2019/index.js
@@ -5,7 +5,7 @@ export function Year2019() {
   return (
     <>
       <DateEntry title="2019" />
-      <TimelineEntry title="Started as NAVER intern" timestamp="Jul 1st, 2019 - Soeul, South Korea" Icon={Briefcase} tint="indigo">
+      <TimelineEntry title="Started as NAVER intern" timestamp="Jul 1st, 2019 - Seoul, South Korea" Icon={Briefcase} tint="indigo">
         <Notes>
           <p>Dubbed as the Google of Korea. The country's number one search engine. </p>
           <p>As a developer in the AI Lab Team, our main goal is to provide information and services that users need even before they request them. </p>
